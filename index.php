80b541176ed338e3988ebceb5e8e9d99dca9a1ab
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-09 15:35:08 +0100

    bdd connexion

--- index.php
+++ index.php
@@ -23,10 +23,26 @@
                 <fieldset>
                     <legend>Connexion</legend>
                     <input type="text" id="login" name="login" placeholder="Identifiant"><br>
-                    <input type="password" id="password" name="login" placeholder="Mot de passe"><br>
-                    <button type="button" class="btn btn-outline-light btn-lg btnConnexionF" >Connexion</button>
+                    <input type="password" id="password" name="password" placeholder="Mot de passe"><br>
+                    <button type="submit" class="btn btn-outline-light btn-lg btnConnexionF" >Connexion</button>
                 </fieldset>
-            </form>
+            </form><br>
+            <?php
+                    session_start();
+                    if(isset($_POST['login']) && isset($_POST['password']))
+                    {
+                        /*----Connexion à la BDD----*/
+                        $db_root = 'root';
+                        $db_pass = '';
+                        $db_name = 'webmaster';
+                        $db_host = 'localhost';
+                        $db_port = 3306;
+                        $db = mysqli_connect($db_host, $db_root, $db_pass, $db_name, $db_port) or die('peut pas se co');
+                    }
+                    /*----Elimination de toute les attaque de type SQL Injection et XSS----*/
+                    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+                    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+                ?>
         </div>
     </div>
     <script type='text/javascript' src='./assets/vendors/jquery/jquery-ui.min.js'></script>
