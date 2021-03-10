c272b151f4d8b1b78b64a867adaed114c74811af
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-10 16:30:08 +0100

    ajout de header.php

--- home.php
+++ home.php
@@ -1,57 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/responsive.css">
-    <link rel="stylesheet" href="./assets/css/anime.css">
-    <script src="https://kit.fontawesome.com/0c87a70838.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="./assets/css/responsive.css">
+        <link rel="stylesheet" href="./assets/css/anime.css">
+        <script src="https://kit.fontawesome.com/0c87a70838.js"></script>
+    </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-md navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php"><img class ="logo"src="assets/images/logo.png"></a>
-                    <div class ="d-md-none mobile" data-toggle="collapse" data-target="#navbarResponsive">
-                        <div class = 'bg-dark line1' data-toggle="collapse" data-target="#navbarResponsive"></div>
-                        <div class = 'bg-dark line2' data-toggle="collapse" data-target="#navbarResponsive"></div>
-                        <div class = 'bg-dark line3' data-toggle="collapse" data-target="#navbarResponsive"></div>
-                    </div>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="home.php">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="account.php">Account</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Avis</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <div class="recherche_p">
-                <form class="form-inline my-2 my-lg-0">
-                    <div class="recherche-barr">
-                        <input class="recherche-input" type="search" placeholder="Recherche" aria-label="Search">
-                        <a class="recherche-icone">
-                            <i class="fas fa-search"></i>
-                        </a>
-                    </div>
-                </form>
-            </div>
-        </header>
+        <?php include 'header.php'; ?>
         <div class="stages">
             <div class="stage">
                 <h2>Stage N°1</h2>
