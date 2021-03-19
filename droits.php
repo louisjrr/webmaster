ad9ea272f13dba55f3305cf2b6f6402cf65ed6a9
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-19 11:50:22 +0100

    creationde la page des droits des délégué

---
+++ droits.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Needs.com</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="https://kit.fontawesome.com/0c87a70838.js"></script>
+</head>
+<body class="bodyDroits">
+    <section>
+        <form>
+        <div class="block">
+            <h3>Delegate permission :</h3>
+            <hr>
+            <div class="checkbox">
+                <label>SFx2</label>
+                <input type="checkbox">
+                <label>SFx3</label>
+                <input type="checkbox">
+                <label>SFx4</label>
+                <input type="checkbox">
+                <label>SFx5</label>
+                <input type="checkbox">
+                <label>SFx6</label>
+                <input type="checkbox"></br>
+                <label>SFx7</label>
+                <input type="checkbox">
+                <label>SFx8</label>
+                <input type="checkbox">
+                <label>SFx9</label>
+                <input type="checkbox">
+                <label>SFx10</label>
+                <input type="checkbox">
+                <label>SFx11</label>
+                <input type="checkbox"></br>
+                <label>SFx12</label>
+                <input type="checkbox">
+                <label>SFx13</label>
+                <input type="checkbox">
+                <label>SFx14</label>
+                <input type="checkbox">
+                <label>SFx15</label>
+                <input type="checkbox">
+                <label>SFx16</label>
+                <input type="checkbox"></br>
+                <label>SFx17</label>
+                <input type="checkbox">
+                <label>SFx18</label>
+                <input type="checkbox">
+                <label>SFx19</label>
+                <input type="checkbox">
+                <label>SFx20</label>
+                <input type="checkbox">
+                <label>SFx22</label>
+                <input type="checkbox"></br>
+                <label>SFx23</label>
+                <input type="checkbox">
+                <label>SFx24</label>
+                <input type="checkbox">
+                <label>SFx25</label>
+                <input type="checkbox">
+                <label>SFx26</label>
+                <input type="checkbox">
+                <label>SFx32</label>
+                <input type="checkbox"></br>
+                <label>SFx33</label>
+                <input type="checkbox">
+            </div>
+            <button type='submit' class='btnDroits btn-outline-light btn-lg'>Select</button>
+        </div>
+        </form>
+    </section>
+</body>
+</html>
