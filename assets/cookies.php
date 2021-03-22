8731f750c2469aa70cc02bf934874eb3a139086a
Erwan MARTIN <erwan.martin@viacesi.fr>
2021-03-22 14:01:26 +0100

    changements sessions/accounts

---
+++ assets/cookies.php
@@ -0,0 +1,11 @@
+
+<?php
+    setcookie(id, $_SESSION['id']);
+    setcookie(idRole, $_SESSION['idRole']);
+    setcookie(mail, $_SESSION['mail']);
+    setcookie(nom, $_SESSION['nom']);
+    setcookie(prenom, $_SESSION['prenom']);
+    setcookie(age,$_SESSION['age']);
+    setcookie(adresse,$_SESSION['adresse']);
+    setcookie(visible,$_SESSION['visible']);
+?>
