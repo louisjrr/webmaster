dfa7c9b5c86f02a33941c285faa32db6377360f3
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-22 14:20:15 +0100

    creation de delegate.php

--- delegate.php
+++ delegate.php
@@ -1,3 +1,18 @@
 <?php
-    echo $_POST['SFx2'];    
+
+    $right = '1';
+
+    for($i=2;$i<34;$i++){
+        if($_POST['SFx'.$i] == 'on'){
+            $right = $right .'1';
+        }
+        elseif($i == 21 || $i == 27 || $i == 28 || $i == 29 || $i == 30 || $i == 31){
+            continue;
+        }
+        else{
+            $right = $right . '0';
+        }
+    }
+   
 ?>
+
