9f775942a44d0853df3d289565b127bc5fb596b3
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-23 10:26:38 +0100

    bootstrap dans view

--- View/addOffre.php
+++ View/addOffre.php
@@ -8,7 +8,7 @@
         <title>Home</title>
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="../assets/css/style.css">
         <script src="https://kit.fontawesome.com/0c87a70838.js"></script>
     </head>
