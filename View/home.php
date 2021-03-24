91f248e22f543045b48f25fdee2913a4fa182c75
TeoMoca <teo.montouiscalixte@viacesi.fr>
2021-03-24 10:15:06 +0100

    change paths

--- View/home.php
+++ View/home.php
@@ -15,7 +15,7 @@
     <header>
     <nav class="navbar navbar-expand-md navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../View/index.php"><img class ="logo"src="../assets/images/logo.png"></a>
+            <a class="navbar-brand" href="../View/login.php"><img class ="logo"src="../assets/images/logo.png"></a>
             <div class ="d-md-none mobile" data-toggle="collapse" data-target="#navbarResponsive">
                 <div class = 'bg-dark line1' data-toggle="collapse" data-target="#navbarResponsive"></div>
                 <div class = 'bg-dark line2' data-toggle="collapse" data-target="#navbarResponsive"></div>
