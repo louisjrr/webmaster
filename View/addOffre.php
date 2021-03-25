71b20ccb4bbfb6197ab17a2be2a511643adf7deb
Louis JARRIER <louis.jarrier@viacesi.fr>
2021-03-25 11:11:47 +0100

    juste un peu de css

--- View/addOffre.php
+++ View/addOffre.php
@@ -23,14 +23,14 @@
             </div>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
+                    <li class="nav-item">
                         <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="Account">Account</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Avis</a>
+                        <a class="nav-link active" href="#">Avis</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
@@ -40,35 +40,38 @@
         </div>
     </nav>
     </header>
-    <div class="create-stage">
-        <h2 class=titre-stage>Add New Internship offer</h2>
-        <form method='POST'>
-            <fieldset class='form-stage'>
-                <div class="column one">
-                    <input type="text" name="entreprise" placeholder="Nom de l'entreprise" required>
-                    <input type="text" name="titre_stage" placeholder="Intitulé du post" required>
-                    <select name="nb_place" id="nb_place" required>
-                        <option value="" disabled selected value>Place(s) disponible(s)</option>
-                        <option value="1">1 Place</option>
-                        <option value="2">2 Places</option>
-                        <option value="3">3 Places</option>
-                        <option value="4">4 Places</option>
-                    </select>
-                    <input name='stage' type="submit" value="Valider">
-                </div>
-                <div class="column two">
-                    <textarea name="description" style="height: 150px;width:500px;resize: none;" placeholder="Description de l'offre" required></textarea>
-                    <div class="listing">
-                        <?php             
-                            foreach($competences as $n){
-                                echo '<input type="checkbox" value="'.$n["nom_competence"].'"></input><label>'.$n["nom_competence"].'</label>';
-                            }
-                        ?>
+    <section>
+        <div class="create-stage">
+            <h2 class=titre-stage>Add New Internship offer</h2>
+            <form method='POST' class="addIntForm">
+                <fieldset class='form-stage'>
+                    <div class="column one">
+                        <input type="text" name="entreprise" placeholder="Nom de l'entreprise" required>
+                        <input type="text" name="titre_stage" placeholder="Intitulé du post" required>
+                        <select name="nb_place" id="nb_place" required>
+                            <option value="" disabled selected value>Place(s) disponible(s)</option>
+                            <option value="1">1 Place</option>
+                            <option value="2">2 Places</option>
+                            <option value="3">3 Places</option>
+                            <option value="4">4 Places</option>
+                        </select>
+                        <input name='stage' type="submit" value="Valider">
                     </div>
-                </div>
-            </fieldset>
-        </form>
-    </div>
+                    <div class="column two">
+                        <textarea name="description" style="height: 150px;width:500px;resize: none;" placeholder="Description de l'offre" required></textarea>
+                        <div class="listing">
+                            <?php             
+                                foreach($competences as $n){
+                                    echo '<input type="checkbox" value="'.$n["nom_competence"].'"></input><label>'.$n["nom_competence"].'</label>';
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </section>
+    
     <script type='text/javascript' src='./assets/vendors/jquery/jquery-ui.min.js'></script>
     <script type="text/javascript" src="./assets/js/script.js" ></script>
     </body>
